e4f4c52c3df3d29352397da2501db81925d21b33
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-12-16 21:18:53 -0400

    feat: add app header

---
+++ resources/js/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <div class="font-bold text-4xl py-3 border-b border-gray-100">
+            <span class="text-gray-600">Lara</span><span class="text-emerald-600">Blog</span>
+        </div>
+        <div></div>
+    </header>
+</template>
