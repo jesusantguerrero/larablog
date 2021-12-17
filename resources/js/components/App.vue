480970d3298cb38e07d5afd7d99e17b53f78ed36
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-12-17 08:13:46 -0400

    ui: improve post styles

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,45 +1,47 @@
 <template>
-<article class="max-w-6xl mx-auto text-gray-700">
-    <AppHeader />
-    <section class="mt-16 space-y-5">
-        <h2 class="text-4xl font-bold"> Post Title</h2>
-        <div class="flex text-gray-400 space-x-3">
-            <span class="font-bold">{{ metadata.date}}</span>
-            <span>{{ metadata.autorName }}</span>
-            <div class="space-x-1">
-                <span v-for="tag in metadata.tags" class="cursor-pointer text-emerald-500 font-bold hover:text-emerald-400">
-                    #{{ tag }}
-                </span>
+<div class="w-full">
+    <AppHeader class="max-w-6xl mx-auto px-8 md:px-0" />
+    <article class="max-w-6xl mx-auto text-gray-700 px-8 md:px-0">
+        <section class="mt-16 space-y-5 ">
+            <h2 class="text-4xl font-bold"> Post Title</h2>
+            <div class="flex md:flex-row flex-col text-gray-400 md:space-x-3">
+                <span class="font-bold">{{ metadata.date}}</span>
+                <span>{{ metadata.autorName }}</span>
+                <div class="space-x-1">
+                    <span v-for="tag in metadata.tags" class="cursor-pointer text-emerald-500 font-bold hover:text-emerald-400">
+                        #{{ tag }}
+                    </span>
+                </div>
             </div>
-        </div>
 
-        <p class="mt-5">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit non semper, risus pulvinar odio felis montes maecenas auctor sollicitudin in suscipit, nullam lacus sed ligula nam massa pharetra magna. Odio molestie felis enim egestas feugiat taciti quisque tellus purus, ullamcorper ultricies vivamus mus lobortis sociis himenaeos dictumst, libero aptent commodo magnis justo vestibulum eget fusce. Facilisis natoque dis nisi placerat himenaeos taciti fermentum penatibus quam ornare congue inceptos nam purus nascetur, laoreet in eget pellentesque orci diam ad fringilla vitae magna montes sociis commodo eleifend.
-        </p>
+            <p class="mt-5">
+                Lorem ipsum dolor sit amet consectetur adipiscing elit non semper, risus pulvinar odio felis montes maecenas auctor sollicitudin in suscipit, nullam lacus sed ligula nam massa pharetra magna. Odio molestie felis enim egestas feugiat taciti quisque tellus purus, ullamcorper ultricies vivamus mus lobortis sociis himenaeos dictumst, libero aptent commodo magnis justo vestibulum eget fusce. Facilisis natoque dis nisi placerat himenaeos taciti fermentum penatibus quam ornare congue inceptos nam purus nascetur, laoreet in eget pellentesque orci diam ad fringilla vitae magna montes sociis commodo eleifend.
+            </p>
 
-        <p>
-            Pellentesque bibendum habitant mattis purus morbi, nostra mus neque ultrices, facilisis inceptos himenaeos imperdiet. Vulputate vel convallis laoreet dui natoque habitant, placerat ornare ridiculus magnis molestie tempus faucibus, tristique a dignissim lobortis tincidunt. Condimentum est ultricies neque volutpat cum urna lectus eros aliquam integer aptent, rutrum in donec purus mus habitant ridiculus lacinia ante habitasse massa porta, a dictumst laoreet quis diam dictum venenatis lacus convallis egestas.
-        </p>
+            <p>
+                Pellentesque bibendum habitant mattis purus morbi, nostra mus neque ultrices, facilisis inceptos himenaeos imperdiet. Vulputate vel convallis laoreet dui natoque habitant, placerat ornare ridiculus magnis molestie tempus faucibus, tristique a dignissim lobortis tincidunt. Condimentum est ultricies neque volutpat cum urna lectus eros aliquam integer aptent, rutrum in donec purus mus habitant ridiculus lacinia ante habitasse massa porta, a dictumst laoreet quis diam dictum venenatis lacus convallis egestas.
+            </p>
 
-        <p>
-            Pellentesque bibendum habitant mattis purus morbi, nostra mus neque ultrices, facilisis inceptos himenaeos imperdiet. Vulputate vel convallis laoreet dui natoque habitant, placerat ornare ridiculus magnis molestie tempus faucibus, tristique a dignissim lobortis tincidunt. Condimentum est ultricies neque volutpat cum urna lectus eros aliquam integer aptent, rutrum in donec purus mus habitant ridiculus lacinia ante habitasse massa porta, a dictumst laoreet quis diam dictum venenatis lacus convallis egestas.
-        </p>
-    </section>
+            <p>
+                Pellentesque bibendum habitant mattis purus morbi, nostra mus neque ultrices, facilisis inceptos himenaeos imperdiet. Vulputate vel convallis laoreet dui natoque habitant, placerat ornare ridiculus magnis molestie tempus faucibus, tristique a dignissim lobortis tincidunt. Condimentum est ultricies neque volutpat cum urna lectus eros aliquam integer aptent, rutrum in donec purus mus habitant ridiculus lacinia ante habitasse massa porta, a dictumst laoreet quis diam dictum venenatis lacus convallis egestas.
+            </p>
+        </section>
 
-    <section class="mt-5">
-        <h4 class="font-bold text-emerald-500 text-xl">Comments</h4>
-        <CommentBox class="mt-5" @submit="addComment" />
+        <section class="mt-8 py-4 border-t border-gray-100">
+            <h4 class="font-bold text-emerald-500 text-xl">Comments ({{ comments.length }})</h4>
+            <CommentBox class="mt-4" @submit="addComment" />
 
-        <div class="mb-32">
-            <CommentItem
-                :key="comment.id"
-                v-for="comment in comments"
-                :comment="comment"
-                @reply="addComment"
-            />
-        </div>
-    </section>
-</article>
+            <div class="mb-32 mt-8">
+                <CommentItem
+                    :key="comment.id"
+                    v-for="comment in comments"
+                    :comment="comment"
+                    @reply="addComment"
+                />
+            </div>
+        </section>
+    </article>
+</div>
 
 </template>
 
